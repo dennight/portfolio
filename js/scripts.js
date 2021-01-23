aca104eb5cd686a5c83c9705f32d19b69bacc6ba
dennight <dagazdennight@gmail.com>
2021-01-23 23:34:41 +0200

    верстка навигации и шапки

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,23 @@
+"use strict";
 
+// Бургер-меню
+let hamburger = document.querySelector(".hamburger");
+let navbar = document.querySelector(".navbar");
+hamburger.addEventListener("click", function () {
+    hamburger.classList.toggle("is-active");
+    navbar.classList.toggle("is-open");
+});
+
+// Статус пункта меню во время прокрутки страницы
+const links = document.querySelectorAll('.menu-link');
+const sections = document.querySelectorAll('.section');
+
+function changeLinkState() {
+    let index = sections.length;
+    while (--index && window.scrollY + 50 < sections[index].offsetTop) { }
+    links.forEach((link) => link.classList.remove('active'));
+    links[index].classList.add('active');
+}
+
+changeLinkState();
+window.addEventListener('scroll', changeLinkState);
